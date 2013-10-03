ce86df659c48170040ae4814923d647736d6e2f9
David Wilson <daw4475@g.rit.edu>
2013-10-03 14:25:50 -0400

    Namespaces are a thing

--- SleepyScientist/SleepyScientist/EggBeater.cs
+++ SleepyScientist/SleepyScientist/EggBeater.cs
@@ -5,31 +5,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace SleepyScientist.Inventions
+namespace SleepyScientist
 {
-    class RocketSkateboard : Invention
+    class EggBeater : Invention
     {
         /// <summary>
-        /// Constructor for Rocket Skateboard invention. Chains to base
+        /// Constructor for Egg Beater invention. Chains to base
         /// </summary>
-        /// <param name="name">"Rocket Skateboard" - Do we want to hard code this for each invention then pass the hardcoded to the base?</param>
+        /// <param name="name">"Egg Beater" - Do we want to hard code this for each invention then pass the hardcoded to the base?</param>
         /// <param name="max_uses">maximum number of uses per level</param>
         /// <param name="x">Initial X position for invention</param>
         /// <param name="y">Initial Y position for invention</param>
         /// <param name="width">Width of invention</param>
         /// <param name="height">Height of invention</param>
-        public RocketSkateboard(string name, int max_uses, int x, int y, int width, int height)
+        public EggBeater(string name, int max_uses, int x, int y, int width, int height)
             : base(name, max_uses, x, y, width, height)
         {
         }
 
         /// <summary>
-        /// Method that executes the functionality of a Rocket Skateboard
+        /// Method that executes the functionality of an Egg Beater
         /// </summary>
         //public override void Use()
         //{
-        //    Scientist s = new Scientist("", 0, 0, 0, 0);
-        //    s.VeloX *= 3;   // only lasts a certain amount of time, will have to address this
+        //    Scientist s = new Scientist("",0,0,0,0);
+        //    s.Direction *= -1;  // How exactly do we want to do this? Enum could be better, or bool.
 
         //    //base.Use();
         //}
