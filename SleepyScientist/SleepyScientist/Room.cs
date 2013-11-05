e6ca1098343ba7479b5634c786bdf4e909bd8407
John Cognetti <cognettings@gmail.com>
2013-11-05 14:39:21 -0500

    Added method for returning all GameObjects inside of Room.

--- SleepyScientist/SleepyScientist/Room.cs
+++ SleepyScientist/SleepyScientist/Room.cs
@@ -64,6 +64,21 @@ namespace SleepyScientist
 
         public void Draw(SpriteBatch batch) { foreach (Floor floor in this.Floors) { floor.Draw(batch); } }
 
+        /// <summary>
+        /// Grab and return all the GameObjects contained within this Room.
+        /// </summary>
+        /// <returns>Every GameObject on every Floor.</returns>
+        public List<GameObject> GetGameObjects()
+        {
+            List<GameObject> roomObjects = new List<GameObject>();
+            foreach (Floor floor in this.Floors)
+            {
+                roomObjects.AddRange(floor.GetGameObjects());
+            }
+
+            return roomObjects;
+        }
+
         #endregion
     }
 }
